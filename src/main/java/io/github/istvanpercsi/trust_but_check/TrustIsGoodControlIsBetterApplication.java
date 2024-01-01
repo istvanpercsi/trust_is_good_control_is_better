e68f9d62c5caf11efb5b5d01967589f65aaa5549
Istvan Percsi <istvanpercsi@live.com>
2024-01-01 20:46:16 +0100

    Basic spring project

---
+++ src/main/java/io/github/istvanpercsi/trust_but_check/TrustIsGoodControlIsBetterApplication.java
@@ -0,0 +1,13 @@
+package io.github.istvanpercsi.trust_but_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustIsGoodControlIsBetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustIsGoodControlIsBetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/istvanpercsi/trust_but_check/TrustIsGoodControlIsBetterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.istvanpercsi.trust_but_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustIsGoodControlIsBetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
