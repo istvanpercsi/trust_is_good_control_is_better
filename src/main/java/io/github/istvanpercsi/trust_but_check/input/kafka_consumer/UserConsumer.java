0a9670b818117475832fd9979f118b85efd8ba2d
Istvan Percsi <istvanpercsi@live.com>
2024-01-12 23:52:32 +0100

    consumer skeleton

--- src/main/java/io/github/istvanpercsi/trust_but_check/input/kafka_consumer/UserConsumer.java
+++ src/main/java/io/github/istvanpercsi/trust_but_check/input/kafka_consumer/UserConsumer.java
@@ -1,12 +1,18 @@
 package io.github.istvanpercsi.trust_but_check.input.kafka_consumer;
 
+
+import io.github.istvanpercsi.trust_but_check.input.kafka_consumer.model.CreateUserRequest;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+
 public class UserConsumer {
 
 
 
-//    @KafkaListener
-//    public void consumeRecord(ConsumerRecord<String, CreateUserRequest> consumerRecord, Acknowledgment acknowledgment) {
-//        CreateUserRequest createUserRequest = consumerRecord.value();
-//
-//    }
+    @KafkaListener
+    public void consumeRecord(ConsumerRecord<String, CreateUserRequest> consumerRecord, Acknowledgment acknowledgment) {
+        CreateUserRequest createUserRequest = consumerRecord.value();
+
+    }
 }
